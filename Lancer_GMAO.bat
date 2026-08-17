96e799bb4afdba8a08b20edefb282360b8f5e1e0
Charles-Eddy Piot <piot.charly@gmail.com>
2026-08-17 20:55:00 +0200

    Ouverture automatique du navigateur au lancement de l'appli

--- Lancer_GMAO.bat
+++ Lancer_GMAO.bat
@@ -1,8 +1,9 @@
 @echo off
 cd /d "%~dp0"
 echo Demarrage de l'appli GMAO...
-echo Ouvrez http://localhost:5000 dans votre navigateur une fois le serveur demarre.
+echo Le navigateur va s'ouvrir automatiquement dans quelques secondes.
 echo (Fermez cette fenetre pour arreter l'appli)
 echo.
+start "" /min powershell -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Seconds 2; Start-Process 'http://localhost:5000'"
 "%~dp0venv\Scripts\python.exe" "%~dp0run.py"
 pause
